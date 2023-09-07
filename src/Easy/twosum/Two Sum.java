ea47447613d147b0b70841846714b74d8d08639c
Ian <44680390+IanC04@users.noreply.github.com>
2023-09-06 23:52:56 -0500

    Two Sum done

--- src/Easy/twosum/Two Sum.java
+++ src/Easy/twosum/Two Sum.java
@@ -8,8 +8,19 @@ package Easy.twosum;
 import java.util.*;
 
 class Solution {
-    public int solution(int[] args) {
-        int answer = 0;
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] answer = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int other = target - nums[i]
+            if (map.containsKey(other)) {
+                answer[0] = map.get(other);
+                answer[1] = i;
+                break;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
         return answer;
     }
 }
