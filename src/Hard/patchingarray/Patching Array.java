afec41cc1a54da8062e6d6fbe9fc6e66b332b65e
Ian <44680390+IanC04@users.noreply.github.com>
2024-03-23 17:06:09 -0500

    Refactor TODO

---
+++ src/Hard/patchingarray/Patching Array.java
@@ -0,0 +1,28 @@
+/**
+ * @author: Ian Chen
+ * @date: 3/23/2024
+ */
+
+package Hard.patchingarray;
+
+import java.util.*;
+
+/**
+ * @see <a href=https://github.com/IanC04>My GitHub</a>
+ */
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        // Greedy?
+        int patches = 0;
+        for (int i = 1; i <= n; ++i) {
+            boolean possible = false;
+            // TODO
+            if (possible) {
+                continue;
+            }
+            ++patches;
+        }
+
+        return patches;
+    }
+}
--- src/Hard/slidingwindowmaximum/Sliding Window Maximum.java
+++ src/Hard/slidingwindowmaximum/Sliding Window Maximum.java
@@ -1,13 +1,18 @@
-/*
-    Written by Ian Chen on 9/6/2023 with alias ianch
-    GitHub: https://github.com/IanC04
+/**
+ * @author: Ian Chen
+ * @date: 9/6/2023
  */
 
 package Hard.slidingwindowmaximum;
 
+import java.util.*;
+
+/**
+ * @see <a href=https://github.com/IanC04>My GitHub</a>
+ */
 class Solution {
-    public int solution(int[] args) {
-        int answer = 0;
-        return answer;
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // TODO
+        return null;
     }
 }
